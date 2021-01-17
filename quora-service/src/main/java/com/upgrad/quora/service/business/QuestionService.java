5b50144e4a14e18dce112125af9cb02541d8cf61
Shilpa Indolia <shilpaindolia@Shilpas-MacBook-Air.local>
2021-01-18 00:26:14 +0530

    getAllQuestions - Feature implemented to add endpoint (/question/all) to fetch all the questions that have been posted in the application by any user. Any user can access this endpoint

--- quora-service/src/main/java/com/upgrad/quora/service/business/QuestionService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/QuestionService.java
@@ -10,14 +10,17 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.ZonedDateTime;
+import java.util.List;
+
 @Service
 public class QuestionService {
 
     @Autowired
-    private UserDao userDao;
+    private QuestionDao questionDao;
 
     @Autowired
-    private QuestionDao questionDao;
+    private UserDao userDao;
 
     /**
      * Method to persist QuestionEntity object in the database through repository
@@ -45,5 +48,25 @@ public class QuestionService {
         questionEntity.setUser(userAuthEntity.getUser());
         questionDao.createQuestion(questionEntity);
         return questionEntity;
+    }
+
+    /**
+     * Method to retrieve all question posted by any user from the database
+     * @param authorizationToken        -String represents authorization token
+     * @return                          -List of QuestionEntity
+     * @throws AuthorizationFailedException --if incorrect/ invalid authorization code is sent
+     */
+    public List<QuestionEntity> getAllQuestions(final String authorizationToken)throws AuthorizationFailedException {
+        UserAuthEntity userAuthEntity = userDao.getUserAuth(authorizationToken);
+
+        if(userAuthEntity == null){//Checking if user is not signed in.
+            throw new AuthorizationFailedException("ATHR-001","User has not signed in");
+        }else if(userAuthEntity.getLogoutAt() != null){//Checking if user is logged out.
+            throw new AuthorizationFailedException("ATHR-002","User is signed out.Sign in first to get all questions");
+        }
+
+        //Returning the list of questionEntities.
+        List<QuestionEntity> questionEntities = questionDao.getAllQuestions();
+        return questionEntities;
     }
 }
