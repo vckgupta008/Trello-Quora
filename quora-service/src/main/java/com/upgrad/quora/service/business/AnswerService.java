673f4715a5e709319544b3e1ebcc3132059eebbc
Vicky Gupta <vicky.gupta@in.imshealth.com>
2021-01-20 23:32:19 +0530

    editAnswerContent - Feature implemented to add endpoint (/answer/edit/{answerId}) to edit an answer. Only the owner of the answer can edit the answer

--- quora-service/src/main/java/com/upgrad/quora/service/business/AnswerService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/AnswerService.java
@@ -6,6 +6,7 @@ import com.upgrad.quora.service.dao.UserDao;
 import com.upgrad.quora.service.entity.AnswerEntity;
 import com.upgrad.quora.service.entity.QuestionEntity;
 import com.upgrad.quora.service.entity.UserAuthEntity;
+import com.upgrad.quora.service.exception.AnswerNotFoundException;
 import com.upgrad.quora.service.exception.AuthorizationFailedException;
 import com.upgrad.quora.service.exception.InvalidQuestionException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,9 +30,10 @@ public class AnswerService {
      *
      * @param answerEntity       - AnswerEntity object to be persisted in the database
      * @param authorizationToken - String represents authorization token
-     * @param questionUuid - String represents question uuid
+     * @param questionUuid       - String represents question uuid
      * @return - AnswerEntity object
-     * @throws AuthorizationFailedException - if incorrect/ invalid authorization token is sent
+     * @throws AuthorizationFailedException - if incorrect/ invalid authorization token is sent or
+     *                                      user has not signed in or already signed out
      * @throws InvalidQuestionException     - if incorrect/ invalid question uuid is sent
      */
     @Transactional(propagation = Propagation.REQUIRED)
@@ -63,4 +65,51 @@ public class AnswerService {
 
     }
 
+    /**
+     * Method to persist AnswerEntity object in the database through repository
+     *
+     * @param answerEntity       - AnswerEntity object to be persisted in the database
+     * @param authorizationToken - String represents authorization token
+     * @param answerUuid         - String represents answer uuid
+     * @return - AnswerEntity object
+     * @throws AuthorizationFailedException - if incorrect/ invalid authorization token is sent or
+     *                                      Owner does not edit answer or the user has already logged out
+     * @throws AnswerNotFoundException      - if incorrect/ invalid answer uuid is sent
+     */
+    @Transactional(propagation = Propagation.REQUIRED)
+    public AnswerEntity editAnswerContent(final AnswerEntity answerEntity, final String authorizationToken, final String answerUuid)
+            throws AuthorizationFailedException, AnswerNotFoundException {
+
+        UserAuthEntity userAuthEntity = userDao.getUserAuth(authorizationToken);
+
+        // if UserAuthEntity object does not exist for the given authorization code, throw exception
+        if (userAuthEntity == null) {
+            throw new AuthorizationFailedException("ATHR-001", "User has not signed in");
+        }
+
+        // if the user has already logged out, throw exception
+        if (userAuthEntity.getLogoutAt() != null) {
+            throw new AuthorizationFailedException("ATHR-002", "User is signed out.Sign in first to edit an answer");
+        }
+
+        AnswerEntity existingAnswerEntity = answerDao.getAnswerByUuid(answerUuid);
+
+        //if answer does not exist
+        if (existingAnswerEntity == null) {
+            throw new AnswerNotFoundException("ANS-001", "Entered answer uuid does not exist");
+        }
+
+        //if owner of the answer doesn not match with user
+        if (existingAnswerEntity.getUser().getId() != userAuthEntity.getUser().getId()) {
+            throw new AuthorizationFailedException("ATHR-003", "Only the answer owner can edit the answer");
+        }
+
+        answerEntity.setUser(existingAnswerEntity.getUser());
+        answerEntity.setQuestion(existingAnswerEntity.getQuestion());
+        answerEntity.setDate(existingAnswerEntity.getDate());
+        answerEntity.setId(existingAnswerEntity.getId());
+
+        AnswerEntity editedAnswerEntity = answerDao.editAnswerContent(answerEntity);
+        return editedAnswerEntity;
+    }
 }
